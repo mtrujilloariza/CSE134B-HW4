c8a5ec2646849074a00456140c20adc7f7dd4e16
mtrujilloariza <m1trujil@ucsd.edu>
2023-02-28 15:35:18 -0800

    more progress

---
+++ README.md
@@ -0,0 +1,4 @@
+Marlon Trujillo Ariza
+A15997363
+main site : https://tiny-dodol-ac6873.netlify.app/
+native dialogs : https://tiny-dodol-ac6873.netlify.app/nativedialogs.html
--- post.js
+++ post.js
@@ -7,59 +7,63 @@ function init() {
 
 	// load posts from local storage
 	let posts = JSON.parse(localStorage.getItem("posts")) || [];
+	// if its empty, add some dummy posts
 	if (posts.length == []) {
 		posts.unshift({
 			title: "Welcome to my blog!",
-			date: "2020-01-01",
+			date: "2022-02-01",
 			content: "This is my first post. I hope you enjoy it!",
 		});
 
 		posts.unshift({
 			title: "Post 2",
-			date: "2020-01-02",
+			date: "2022-02-02",
 			content: "This is my second post. I hope you enjoy it!",
 		});
 
 		posts.unshift({
 			title: "Post 3",
-			date: "2020-01-03",
+			date: "2020-02-03",
 			content: "This is my third post. I hope you enjoy it!",
 		});
 
 		localStorage.setItem("posts", JSON.stringify(posts));
 	}
 
+	// display posts
 	loadPosts(posts);
 	
+	// when the add post button is clicked, show the dialog with the post form
 	addPostBtn.addEventListener("click", function () {
 		let dialog = document.querySelector("#postDialog");
-		let saveBtn = dialog.querySelector("#savePost");
 		dialog.showModal();
 	});
 
+	// get the cancel button on the post form
 	let cancelPostBtn = document.querySelector("#cancelPost");
-
+	// when clicked cancel, close the dialog
 	cancelPostBtn.addEventListener("click", function () {
 		let dialog = document.querySelector("#postDialog");
 		dialog.close();
 	});
-
+	// get the form on the create post dialog
 	let postForm = document.querySelector("#postForm");
-
+	// when the form is submitted, create a post from the inputs and save it to local storage, then close the dialog
 	postForm.addEventListener("submit", function (e) {
 		e.preventDefault();
+		// get the values from the form
 		let postFormTitle = document.querySelector("#postFormTitle");
 		let postFormDate = document.querySelector("#postFormDate");
 		let postFormContent = document.querySelector("#postFormContent");
-
+		// create a post object
 		let post = {
 			title: postFormTitle.value,
 			date: postFormDate.value,
 			content: postFormContent.value,
 		};
-
+		// reset the form
 		this.reset();
-
+		// close the dialog
 		let dialog = document.querySelector("#postDialog");
 		dialog.close();
 
@@ -67,10 +71,11 @@ function init() {
 		let posts = JSON.parse(localStorage.getItem("posts")) || [];
 		posts.unshift(post);
 		localStorage.setItem("posts", JSON.stringify(posts));
-
+		// reload posts
 		loadPosts(posts);
 	});
 
+	// when the dialog is closed, reset the form
 	let dialog = document.querySelector("#postDialog");
 	dialog.addEventListener("close", function () {
 		document.querySelector("#postForm").reset();
